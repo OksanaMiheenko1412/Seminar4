14b1d8a48e51376b7032c15276a8c8d094c044af
OksanaMiheenko1412 <118578118+OksanaMiheenko1412@users.noreply.github.com>
2022-12-02 23:57:15 +0700

    / add code commit task30

---
+++ task30/Program.cs
@@ -0,0 +1,7 @@
+﻿// task30 Напишите программу,которая выводит массив из 8 элементов,заполненный
+// нулями и единицами в случайном порядке
+Console.Clear();
+int[] array = new int[8];
+for (int i = 0; i < 8; i++)
+    array[i] = new Random().Next(0, 2);
+Console.WriteLine($"Результат:[{string.Join(" , ", array)}]");    
