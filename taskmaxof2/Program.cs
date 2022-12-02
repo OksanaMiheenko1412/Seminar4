765992cfa7c2265eadfd798358740814967232f4
OksanaMiheenko1412 <118578118+OksanaMiheenko1412@users.noreply.github.com>
2022-12-02 22:24:30 +0700

    / add void code commit taskmaxof2

--- taskmaxof2/Program.cs
+++ taskmaxof2/Program.cs
@@ -1,14 +1,11 @@
 ﻿// taskmaxof2 найти максимум среди 2 чисел
 
-int maxNumbers(int n, int m)// int - возвращаемый тип данных: или n или m
+void maxNumbers(int n)//  - процедура
 {
-    if (n > m)
-        return n;
-    return m;    
+    n = n + 10;  
 }
 Console.Clear();
-Console.Write("Введите 1-ое число: ");
+Console.Write("Введите число: ");
 int n = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите 2-ое число: ");
-int m = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine($"Результат: {maxNumbers(n, m)}");
+maxNumbers(n);
+Console.WriteLine($"Результат: {n}");
