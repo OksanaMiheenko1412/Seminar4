5b7e24dad90d17c7972e43e2744e15157e8a89a2
OksanaMiheenko1412 <118578118+OksanaMiheenko1412@users.noreply.github.com>
2022-12-02 23:37:57 +0700

    / add code commit task28

---
+++ task28/Program.cs
@@ -0,0 +1,8 @@
+﻿// task28 - программа на вход принимает число N и выдает произведение чисел от
+// 1 до N, например: 4 ->24; 5 ->120, нахождение факториала
+Console.Clear();
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine()), result = 1;
+for (int i = 1; i <= n; i++)
+    result *= i; // result = result * i
+Console.WriteLine($"Результат: {result}");    
