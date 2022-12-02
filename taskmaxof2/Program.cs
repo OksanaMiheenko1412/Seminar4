a5972630c7a666789505a853f32f046c1530584b
OksanaMiheenko1412 <118578118+OksanaMiheenko1412@users.noreply.github.com>
2022-12-02 22:29:18 +0700

    / add  ref commit taskmaxof2

--- taskmaxof2/Program.cs
+++ taskmaxof2/Program.cs
@@ -1,11 +1,11 @@
 ﻿// taskmaxof2 найти максимум среди 2 чисел
 
-void maxNumbers(int n)//  - процедура
+void maxNumbers(ref int n)//  - процедура
 {
     n = n + 10;  
 }
 Console.Clear();
 Console.Write("Введите число: ");
 int n = Convert.ToInt32(Console.ReadLine());
-maxNumbers(n);
+maxNumbers(ref n);
 Console.WriteLine($"Результат: {n}");
